3bb360661e9fc64c1008a7281620bdeb5e388d50
Jenny Moorehead <jennym255@gmail.com>
2026-04-10 18:00:24 +0100

    feat(BE_002): Implement GET /api/matches with specific match id

--- backend/python-flask/app/routes/matches.py
+++ backend/python-flask/app/routes/matches.py
@@ -51,8 +51,9 @@ def get_matches():
         query = query.filter_by(city_id=city)
     
     if date is not None:
-        # Validate date format (YYYY-MM-DD)
+        # Validate date format (YYYY-MM-DD) using regex
         if not re.match(r'^\d{4}-\d{2}-\d{2}$', date):
+            # error handling if format is invalid
             return jsonify({"error": "Invalid date format. Use YYYY-MM-DD"}), 400
         
         # Filter for dates that start with the provided date
@@ -77,5 +78,22 @@ def get_matches():
 
 @matches_bp.route('/<id>', methods=['GET'])
 def get_match_by_id(id):
-    # TODO: Replace with your implementation (YOUR TASK #2)
-    return jsonify({}), 200
+    """
+    GET endpoint to retrieve a single match by ID.
+    
+    Args:
+        id (str): The match ID
+    
+    Returns:
+        JSON object of the match if found
+        404 error if match not found
+    """
+    # query for the match by it's id
+    match = Match.query.get(id)
+
+    # return 404 error if the match wasn't found
+    if match is None:
+        return jsonify({"error":f"Match with id: '{id}' not found" }), 404
+
+    # else return the match as JSON
+    return jsonify(match.to_dict()), 200
